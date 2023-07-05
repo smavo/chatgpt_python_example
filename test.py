a79b14d9b99229106a08c4c8d2ea63a3aef4eda0
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-07-04 22:00:11 -0500

    Primer ejemplo

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 import os
+import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = api_key
+
+# modelos = openai.Model.list()
+# print(modelos)
+
+modelo = 'text-davinci-002'
+pregunta = 'Puedes crear un poema de 15 palabras sobre el amor'
+
+respuesta = openai.Completion.create(
+    engine=modelo,
+    prompt=pregunta,
+    n=1,
+    temperature=0.1
+)
+
+texto_generado = respuesta.choice[0].text.strip()
+
+print(texto_generado)
